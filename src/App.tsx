03a3ac98adc85f95473c2e87110f0aaa27afc544
budhesh22 <budhesh.inexture@gmail.com>
2022-12-22 12:04:20 +0530

    feat: created a new scretch project with typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HelloWord from "./component/HelloWord";
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWord />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/HelloWord.tsx
@@ -0,0 +1,9 @@
+const HelloWord = () => {
+  return (
+    <div>
+      <h1>HelloWorld</h1>
+    </div>
+  );
+};
+
+export default HelloWord;
